9b5fb4440b9a90a7f9462c58af7fd65ca0d0b1cd
arkadius <arkadius.kraus@gmail.com>
2017-11-13 02:26:46 +0100

    part 1.2

---
+++ app/src/main/java/pl/arkadius/testapp/MainActivity.java
@@ -0,0 +1,21 @@
+package pl.arkadius.testapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openButton(View v){
+        Intent toA2 = new Intent(this,Activity2.class);
+        startActivity(toA2);
+    }
+}
